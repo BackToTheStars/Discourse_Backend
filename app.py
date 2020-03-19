03b25b36ccad8e4b0c57253b5b2e2aee3410202b
BackToTheStars <nikgrigoryev@gmail.com>
2020-03-18 21:44:46 -0700

    Fix not importing db

--- app.py
+++ app.py
@@ -21,11 +21,6 @@ app.secret_key = 'app'                          # should be long and secure
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-  db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)          # creates new endpoint /auth
 
 
